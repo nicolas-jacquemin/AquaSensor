b8582ba9c27bd9188ac66411d2af65622f02d598
nicojqn <jacquemin.nicolas59@gmail.com>
2023-06-01 19:03:29 +0200

    [+] WEB - Loading module

---
+++ webapp/components/Loading.vue
@@ -0,0 +1,19 @@
+<template>
+    <VDialog v-model="loading" width="auto">
+      <VCard>
+        <VCardText>
+            <VProgressCircular indeterminate color="primary"></VProgressCircular>
+        </VCardText>
+      </VCard>
+    </VDialog>
+</template>
+
+<script setup lang="ts">
+
+const loading = ref(false);
+
+onMounted(() => {
+    loading.value = true;
+})
+
+</script>
