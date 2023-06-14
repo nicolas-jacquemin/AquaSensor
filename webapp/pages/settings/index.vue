c9bc017e8f00237e5c31025f437a6cca5183cb56
nicolas-jacquemin <nicolas1.jacquemin@epitech.eu>
2023-06-14 16:42:21 +0200

    [~] Settings router menu

--- webapp/pages/settings/index.vue
+++ webapp/pages/settings/index.vue
@@ -1,11 +1,40 @@
 <template>
-
+  <VContainer fluid class="mx-auto justify-center">
+    <VRow>
+      <VCol></VCol>
+      <VCol cols="12" sm="6" md="4" lg="3">
+        <VCard>
+          <VCardText>
+            <VList>
+              <VListItem
+                v-for="item in items"
+                :prepend-icon="item.icon"
+                :title="item.text"
+                :value="item.value"
+                @click="router.push(`${item.to}`)"
+                nav
+              ></VListItem>
+            </VList>
+          </VCardText>
+        </VCard>
+      </VCol>
+      <VCol></VCol>
+    </VRow>
+  </VContainer>
 </template>
 
 <script setup>
-
 definePageMeta({
-    layout: "appbar"
-})
+  layout: "appbar",
+});
 
+const router = useRouter();
+
+const items = ref([
+  {
+    icon: "fluent:person-24-regular",
+    text: "Account",
+    to: "/settings/account",
+  },
+]);
 </script>
