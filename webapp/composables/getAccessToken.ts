bcda8d0e10ba419ded3b5bd88190ee03f0a2d3b5
nicolas-jacquemin <nicolas1.jacquemin@epitech.eu>
2023-06-15 11:48:44 +0200

    [~] Popup on network error

--- webapp/composables/getAccessToken.ts
+++ webapp/composables/getAccessToken.ts
@@ -26,33 +26,34 @@ export default async function getAccessToken(): Promise<string> {
         setStorage(userData.data.email, userData.data.icon, userData.data.name, userData.data.slug, userData.data.permissions);
         return localStorage.getItem("token")!;
     } catch (e: any) {
-        if (e.message === "Unknown error")
-            throw new Error("Unknown error");
-        if (localStorage.getItem("refreshToken") === null)
-            throw new Error("Invalid token");
-        let renew = await fetch("/api/auth/renewToken", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                refreshToken: localStorage.getItem("refreshToken")
+        if (e.message === "Invalid Token") {
+            let renew = await fetch("/api/auth/renewToken", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    refreshToken: localStorage.getItem("refreshToken")
+                })
             })
-        })
-        if (renew.status === 401 || renew.status === 400) {
+            if (renew.status === 401 || renew.status === 400) {
+                throw new Error("Invalid token");
+            } else if (renew.status !== 200) {
+                throw new Error("Unknown error");
+            }
+            let json = await renew.json();
+            if (json.data.token === undefined || json.data.refreshToken === undefined) {
+                throw new Error("Invalid token");
+            }
+            localStorage.setItem("token", json.data.token);
+            localStorage.setItem("expUTC", json.data.expUTC);
+            localStorage.setItem("refreshToken", json.data.refreshToken);
+            localStorage.setItem("refreshExpUTC", json.data.refreshExpUTC);
+            setStorage(json.data.email, json.data.icon, json.data.name, json.data.slug, json.data.permissions);
+            return json.data.token;
+        } else if (localStorage.getItem("refreshToken") === null)
             throw new Error("Invalid token");
-        } else if (renew.status !== 200) {
+        else
             throw new Error("Unknown error");
-        }
-        let json = await renew.json();
-        if (json.data.token === undefined || json.data.refreshToken === undefined) {
-            throw new Error("Invalid token");
-        }
-        localStorage.setItem("token", json.data.token);
-        localStorage.setItem("expUTC", json.data.expUTC);
-        localStorage.setItem("refreshToken", json.data.refreshToken);
-        localStorage.setItem("refreshExpUTC", json.data.refreshExpUTC);
-        setStorage(json.data.email, json.data.icon, json.data.name, json.data.slug, json.data.permissions);
-        return json.data.token;
     }
 }
